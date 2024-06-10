78e3459b74dcf529eccee1db214052570841b475
krishyadav123 <krishyadav28494@gmail.com>
2024-06-10 19:03:05 +0530

    change2

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -19,7 +19,7 @@ import { login } from "../../redux/actions/auth";
 
 //Configure toastify
 toast.configure();
-console.log("one")
+console.log("hello")
 
 const Login = () => {
   let navigate = useNavigate();
